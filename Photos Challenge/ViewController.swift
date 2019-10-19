f794fa002a9cf132379c028c2b4d01f0b4f88e4e
Carmel Braga <carmelbraga@hotmail.com>
2019-10-18 22:21:21 -0500

    Initial Commit

---
+++ Photos Challenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Photos Challenge
+//
+//  Created by Carmel Braga on 10/18/19.
+//  Copyright © 2019 Carmel Braga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
