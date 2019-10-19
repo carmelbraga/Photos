55cee31da20f367fc87ff5b0c22f24a684708b2b
Carmel Braga <carmelbraga@hotmail.com>
2019-10-18 22:49:44 -0500

    First commit

--- Photos Challenge/ViewController.swift
+++ Photos Challenge/ViewController.swift
@@ -8,13 +8,81 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func takePhoto(_ sender: Any) {
+    let imagePickerController = UIImagePickerController()
 
+           imagePickerController.delegate = self
+
+                 let actionList = UIAlertController(title: "", message: nil, preferredStyle: .actionSheet)
+
+                 actionList.addAction(UIAlertAction(title: "Take Photo", style: .default, handler: {(action: UIAlertAction) in imagePickerController.sourceType = .camera
+
+                     if UIImagePickerController.isSourceTypeAvailable(.camera){
+
+                         imagePickerController.sourceType = .camera
+
+                         self.present(imagePickerController, animated: true, completion: nil)
+
+                     } else{
+
+                         print("Camera not available.")
+
+                     }
+
+                 }))
+
+                 actionList.addAction(UIAlertAction(title: "Cancel", style: .default, handler: nil))
+
+                 self.present(actionList, animated: true, completion: nil)
+    }
+    
+    @IBAction func choosePhoto(_ sender: Any) {
+        
+        let imagePickerController = UIImagePickerController()
+
+                      imagePickerController.delegate = self
+
+                      
+
+                      let actionList = UIAlertController(title: "", message: nil, preferredStyle: .actionSheet)
+
+                      
+
+                      actionList.addAction(UIAlertAction(title: "Choose Photo", style: .default, handler: {(action: UIAlertAction) in imagePickerController.sourceType = .photoLibrary
+
+                          
+
+                          self.present(imagePickerController, animated: true, completion: nil)
+
+                      }))
+
+                      actionList.addAction(UIAlertAction(title: "Cancel", style: .default, handler: nil))
+
+                      
+
+                      self.present(actionList, animated: true, completion: nil)
+
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            let image = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+
+                  imageView.image = image
+
+                  picker.dismiss(animated: true, completion: nil)
+       }
+       
+       func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+           picker.dismiss(animated: true, completion: nil)
+       }
 }
 
